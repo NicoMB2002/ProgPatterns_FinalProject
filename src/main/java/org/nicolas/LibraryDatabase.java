b18dcdb54884bc672f6b47c58f02678fe2db73db
nico <nicomoncayo@hotmail.com>
2025-04-14 14:06:45 -0400

    Added createTables() method to initialize the database

--- src/main/java/org/nicolas/LibraryDatabase.java
+++ src/main/java/org/nicolas/LibraryDatabase.java
@@ -3,6 +3,7 @@ package org.nicolas;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.*;
 
 public class LibraryDatabase {
 
@@ -18,6 +19,54 @@ public class LibraryDatabase {
             throw new RuntimeException(e);
         }
         return connection;
+    }
+
+    public static void createTables() {
+        String userTable = """
+        CREATE TABLE IF NOT EXISTS User (
+            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            role TEXT NOT NULL,
+            password TEXT NOT NULL
+        );
+        """;
+
+        String booksTable = """
+        CREATE TABLE IF NOT EXISTS Books (
+            isbn TEXT PRIMARY KEY,
+            title TEXT NOT NULL,
+            author TEXT NOT NULL,
+            no_copies INTEGER NOT NULL,
+            borrowed_books INTEGER NOT NULL,
+            available_copies INTEGER NOT NULL
+        );
+        """;
+
+        String borrowedBooksTable = """
+        CREATE TABLE IF NOT EXISTS BorrowedBooks (
+            borrowedBook_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            book_id TEXT,
+            borrow_date TEXT,
+            return_date TEXT,
+            return_status TEXT,
+            FOREIGN KEY(user_id) REFERENCES User(user_id),
+            FOREIGN KEY(book_id) REFERENCES Books(isbn)
+        );
+        """;
+
+        try {
+            Connection conn = connect();
+            Statement stmt = conn.createStatement();
+            //execute the Create table statements
+            stmt.execute(userTable);
+            stmt.execute(booksTable);
+            stmt.execute(borrowedBooksTable);
+            System.out.println("\nTables created successfully.\n");
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     //Testing connectivity
