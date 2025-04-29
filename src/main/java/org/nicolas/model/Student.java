6538e47bef6165f6fd7e4e96843644f4552e4cd1
ash-a9236 <helene.m.rousseau@gmail.com>
2025-04-29 11:19:32 -0400

    adding borrowedBooks arrayList<Books> in data fields + adding getters & setters

--- src/main/java/org/nicolas/model/Student.java
+++ src/main/java/org/nicolas/model/Student.java
@@ -9,10 +9,44 @@ public class Student extends User {
     private int userId;
     private String name;
     private UserType typeOfUSer;
+    private ArrayList<Book> borrowedBooks;
 
     public Student(int userID, String name, String password) {
         super(userID, name, password);
+        this.borrowedBooks = new ArrayList<>();
     }
 
-    public void seeBorrowedBooks () {}
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UserType getTypeOfUSer() {
+        return typeOfUSer;
+    }
+
+    public void setTypeOfUSer(UserType typeOfUSer) {
+        this.typeOfUSer = typeOfUSer;
+    }
+
+    public ArrayList<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(ArrayList<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
 }
